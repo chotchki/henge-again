33c9fed630288aaf62e8d118ebe6ee02460253fe
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2014-11-28 23:38:08 -0500

    Finished adding video support

--- api/cron.php
+++ api/cron.php
@@ -1,18 +1,23 @@
 <?php
 //Regenerate all .albumInfo files
 
-require_once dirname(__FILE__) . "photoFunctions.php";
+require_once dirname(__FILE__) . "/functions/album.php";
+require_once dirname(__FILE__) . "/functions/photo.php";
 
-$album_list = getSimpleAlbumList("/");
+$album_list = getListOfAlbums();
 
 foreach($album_list as $album){
 	$album_contents_raw = getAlbumInfo($album);
 	
-	$album_contents = json_decode($album_contents_raw);
+	$album_contents = json_decode($album_contents_raw, true);
 	if($album_contents == null){
 		continue;
 	}
 	
-	print_r($album_contents);
+	foreach($album_contents["items"] as $item){
+		getThumbRealPath($item["name"], 150, 225);
+		getThumbRealPath($item["name"], 300, 450);
+		getThumbRealPath($item["name"], 600, 900);
+	}
 }
 ?>
--- api/functions/photo.php
+++ api/functions/photo.php
@@ -3,42 +3,76 @@
 require_once dirname(__FILE__) . "/../../settings.php";
 require_once dirname(__FILE__) . "/path.php";
 
+function isMovie($name){
+	return preg_match("/(avi|mov|mp4)$/i", $name);
+}
+
+function isPicture($name){
+	return preg_match("/(arw|cr2|jpg|jpeg|png|thm|tif|tiff)$/i", $name);
+}
+
 function getItemDateTime($path){
 	if(!isSafePath($path)){
 		return false;
 	}
+	
+	if(isPicture($path)){
+		exec('identify -format "%[EXIF:DateTime]" ' . escapeshellarg($path), $output, $return_code);
 
-	exec('identify -format "%[EXIF:DateTime]" ' . escapeshellarg($path), $output, $return_code);
+		if($return_code != 0 || !$output){
+			return false;
+		}
 
-	if($return_code != 0 || !$output){
+		return date_create_from_format('Y:n:d G:i:s', $output[0]);
+	} elseif(isMovie($path)){
+		exec('ffprobe -show_entries "format_tags=creation_time" -print_format compact=nk=1:p=0 -loglevel error ' . escapeshellarg($path), $output, $return_code);
+		
+		if($return_code != 0 || !$output){
+			return false;
+		}
+		
+		return date_create_from_format('Y-n-d G:i:s', $output[0]);
+	} else {
 		return false;
 	}
-
-	return date_create_from_format('Y:n:d G:i:s', $output[0]);
 }
 
 function getItemSize($path){
 	if(!isSafePath($path)){
 		return false;
 	}
+
+	if(isPicture($path)){
+		exec('identify -format "%Wx%Hx%[exif:orientation]" ' . escapeshellarg($path), $output, $return_code);
 	
-	exec('identify -format "%Wx%Hx%[exif:orientation]" ' . escapeshellarg($path), $output, $return_code);
+		if($return_code != 0 || !$output){
+			return false;
+		}
 	
-	if($return_code != 0 || !$output){
+		list($width, $height, $orient) = split("x", $output[0]);
+	
+		switch($orient){
+			case 1:
+			case 3:
+				return array($width, $height);
+			case 6:
+			case 8:
+				return array($height, $width);
+			default:
+				return array($width, $height);
+		}
+	} elseif(isMovie($path)){
+		exec('ffprobe -select_streams v:0 -show_entries stream=height,width -print_format compact=nk=1:p=0 -loglevel error ' . escapeshellarg($path), $output, $return_code);
+		
+		if($return_code != 0 || !$output){
+			return false;
+		}
+		
+		list($width, $height) = split("|", $output[0]);
+		
+		return array($width, $height);
+	} else {
 		return false;
-	}
-	
-	list($width, $height, $orient) = split("x", $output[0]);
-	
-	switch($orient){
-		case 1:
-		case 3:
-			return array($width, $height);
-		case 6:
-		case 8:
-			return array($height, $width);
-		default:
-			return array($width, $height);
 	}
 }
 
@@ -72,17 +106,29 @@ function getThumbRealPath($name, $height, $width){
 	}
 	
 	if($thumb_exists === false || $thumb_mod < $source_mod){
-		$cmd = "convert -auto-orient " . escapeshellarg($source) . " " . escapeshellarg($thumb);
-		$output = system($cmd, $status);
-		if($status != 0){
-			error_log("Failed to auto orient " . $cmd . " | " . $status . " | " . $output);
-			return false;
-		}
-	
-		$cmd = "convert -geometry " . "x" . $height . " " . escapeshellarg($thumb) . " " . escapeshellarg($thumb);
-		$output = system($cmd, $status);
-		if($status != 0){
-			error_log("Failed to resize " . $cmd . " | " . $status . " | " . $output);
+		if(isPicture($source)){ //Convert pictures
+			$cmd = "convert -auto-orient " . escapeshellarg($source) . " " . escapeshellarg($thumb);
+			$output = system($cmd, $status);
+			if($status != 0){
+				error_log("Failed to auto orient " . $cmd . " | " . $status . " | " . $output);
+				return false;
+			}
+			
+			$cmd = "convert -geometry " . "x" . $height . " " . escapeshellarg($thumb) . " " . escapeshellarg($thumb);
+			$output = system($cmd, $status);
+			if($status != 0){
+				error_log("Failed to resize " . $cmd . " | " . $status . " | " . $output);
+				return false;
+			}	
+		} elseif(isMovie($source)){ //Convert movies
+			$cmd = "ffmpeg -loglevel error -i " . escapeshellarg($source) . ' -vf "thumbnail,scale="' . $width . ":" . $height . '" -frames:v 1 ' . escapeshellarg($thumb);
+			$output = system($cmd, $status);
+			if($status != 0){
+				error_log("Failed to resize " . $cmd . " | " . $status . " | " . $output);
+				return false;
+			}
+		} else {
+			error_log("Unable to convert " . $source);
 			return false;
 		}
 	}
